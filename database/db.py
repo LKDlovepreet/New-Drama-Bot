409b47ea95be2e3fd6553f75b75b862c7d2a9f83
Lovepreet Singh <singhlovepreet05198@gmail.com>
2026-01-08 16:47:05 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,29 @@
 #database
+
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import Base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Database Switcher (Cloud vs Local)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///storage.db")
+
+# Koyeb fix: Postgres URL 'postgresql://' hona chahiye
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL, echo=False)
+SessionLocal = sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
