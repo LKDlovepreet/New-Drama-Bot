b1141a531b034fa33dec42e029bfeb054332fdb4
Lovepreet Singh <singhlovepreet05198@gmail.com>
2026-01-08 16:46:42 +0530

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,1 +1,16 @@
 #table ka design 
+
+from sqlalchemy import Column, Integer, String, BigInteger
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class FileRecord(Base):
+    __tablename__ = "files"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    unique_token = Column(String, unique=True, index=True)
+    file_id = Column(String)
+    file_name = Column(String)
+    file_type = Column(String)  # doc, video, photo
+    uploader_id = Column(BigInteger)
