82cc263664ce90ec7fd80e99d0cae12a69378d03
Lovepreet Singh <singhlovepreet05198@gmail.com>
2026-01-11 18:17:54 +0530

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,10 +1,10 @@
-#table ka design 
-
-from sqlalchemy import Column, Integer, String, BigInteger
+from sqlalchemy import Column, Integer, String, BigInteger, Boolean, DateTime # 👈 Yaha DateTime add kiya hai
 from sqlalchemy.orm import declarative_base
+from datetime import datetime # 👈 Ye line bhi zaroori hai
 
 Base = declarative_base()
 
+# 1. File Record Table
 class FileRecord(Base):
     __tablename__ = "files"
     
@@ -15,6 +15,7 @@ class FileRecord(Base):
     file_type = Column(String)  # doc, video, photo
     uploader_id = Column(BigInteger)
 
+# 2. Channel Table
 class Channel(Base):
     __tablename__ = "channels"
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -22,9 +23,10 @@ class Channel(Base):
     channel_name = Column(String)
     added_by = Column(BigInteger)
 
+# 3. Users Table (Isme Error aa raha tha)
 class BotUser(Base):
     __tablename__ = "bot_users"
     
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(BigInteger, unique=True, index=True) # User ka Telegram ID
-    joined_date = Column(DateTime, default=datetime.utcnow)
+    user_id = Column(BigInteger, unique=True, index=True)
+    joined_date = Column(DateTime, default=datetime.utcnow) # 👈 Ab ye chal jayega
