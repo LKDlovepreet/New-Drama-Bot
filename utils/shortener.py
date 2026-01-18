1259994547e788b855bfa59ddd5b65df3c157095
Lovepreet Singh <singhlovepreet05198@gmail.com>
2026-01-18 11:56:13 +0530

    Update shortener.py

--- utils/shortener.py
+++ utils/shortener.py
@@ -1,15 +1,15 @@
 from curl_cffi.requests import AsyncSession
-from config.settings import GPLINKS_API
+from config.settings import SHORTENER_API
 
 async def get_short_link(long_url):
     try:
         # 👇 FIX: Check karein ki Key Environment se load hui ya nahi
-        if not GPLINKS_API:
-            print("❌ Error: GPLinks API Key set nahi hai Environment Variables mein!")
+        if not SHORTENER_API:
+            print("❌ Error: SHORTENER API Key set nahi hai Environment Variables mein!")
             return None
 
         # GPLinks API URL
-        api_url = f"https://gplinks.in/api?api={GPLINKS_API}&url={long_url}&format=text"
+        api_url = f"https://mdiskshortner.link/api?api={GPLINKS_API}&url={long_url}&format=text"
         
         # Chrome ban kar request bhejein
         async with AsyncSession(impersonate="chrome110") as session:
@@ -19,14 +19,14 @@ async def get_short_link(long_url):
                 result = response.text.strip()
                 
                 # Debugging ke liye
-                print(f"🔗 GPLinks Response: {result[:50]}") 
+                print(f"🔗 mdiskshortner  Response: {result[:50]}") 
 
                 # Agar link 'http' se shuru ho raha hai to sahi hai
                 if result.startswith("http"):
                     return result
                 else:
                     # Agar koi error message aaya
-                    print(f"❌ GPLinks Error: {result}")
+                    print(f"❌ mdiskshortner Error: {result}")
                     return None
             else:
                 print(f"❌ HTTP Error: {response.status_code}")
