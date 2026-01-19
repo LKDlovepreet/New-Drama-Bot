310e4d3e0ccc1542f90644baafadf18533ca1049
Lovepreet Singh <singhlovepreet05198@gmail.com>
2026-01-19 15:51:32 +0530

    Update states.py

--- utils/states.py
+++ utils/states.py
@@ -1,9 +1,9 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class PostWizard(StatesGroup):
-waiting_for_media = State()
-waiting_for_caption = State()
-waiting_for_buttons = State()
-waiting_for_timer = State()
-waiting_for_target = State()
-confirmation = State()
+     waiting_for_media = State()
+     waiting_for_caption = State()
+     waiting_for_buttons = State()
+     waiting_for_timer = State()
+     waiting_for_target = State()
+     confirmation = State()
