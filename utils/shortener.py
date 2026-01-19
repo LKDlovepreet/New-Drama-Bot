9a0bdf2d21abea19f70712c2f0cdda1551bd392a
Lovepreet Singh <singhlovepreet05198@gmail.com>
2026-01-19 15:41:40 +0530

    Update shortener.py

--- utils/shortener.py
+++ utils/shortener.py
@@ -3,23 +3,20 @@ from config.settings import SHORTENER_API
 
 async def get_short_link(long_url):
     try:
-        # 👇 FIX: Check karein ki Key Environment se load hui ya nahi
+
         if not SHORTENER_API:
             print("❌ Error: SHORTENER API Key set nahi hai Environment Variables mein!")
             return None
 
-        # GPLinks API URL
-        api_url = f"https://gplinks.in/api?api={SHORTENER_API}&url={long_url}&format=text"
-        
-        # Chrome ban kar request bhejein
+        api_url = f"https://shrinkearn.com/api?api={SHORTENER_API}&url={long_url}&format=text"
+
         async with AsyncSession(impersonate="chrome110") as session:
             response = await session.get(api_url)
             
             if response.status_code == 200:
                 result = response.text.strip()
-                
-                # Debugging ke liye
-                print(f"🔗 gplinks  Response: {result[:50]}") 
+
+                print(f"🔗 shrinkearn  Response: {result[:50]}") 
 
                 # Agar link 'http' se shuru ho raha hai to sahi hai
                 if result.startswith("http"):
